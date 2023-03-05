017e46f31626bcdd33b37b688f23e2d70e94f249
Taylor <92060690+TacoError@users.noreply.github.com>
2023-03-05 19:49:35 +0000

    instanced rendering, archiving project and restart

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import BlockRegistry from "./blocks/block_registry";
 import World from "./world/world";
 import FlatGenerator from "./world/generator/types/flat_generator";
 import Vector2 from "./math/vector2";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import TextureLoader from "./utils/textures";
- try {
+import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls"
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
 let camera: THREE.Camera;
 let world: World;
-let controls: OrbitControls;
+let fpc: FirstPersonControls;
 
 function initialize() : void {
     TextureLoader.initialize();
@@ -21,24 +20,27 @@ function initialize() : void {
     camera.position.z = 5;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    controls = new OrbitControls(camera, renderer.domElement);
     document.body.appendChild(renderer.domElement);
-    for (let x = -5; x <= 5; x++) {
-        for (let z = -5; z <= 5; z++) {
-            world.generate(x, z);
-            world.getChunk(new Vector2(x, z)).addToScene(scene);
-        }
-    }
+    fpc = new FirstPersonControls(camera, renderer.domElement);
+    world.generate(0, 0);
+    world.getChunk(new Vector2(0, 0)).addToScene(scene);
 }
+
+let keysPressed: string[] = [];
+document.addEventListener("keydown", (ev: KeyboardEvent) => {
+    keysPressed.push(ev.key.toLowerCase());
+});
+document.addEventListener("keyup", (ev: KeyboardEvent) => {
+    keysPressed = keysPressed.filter(key => key !== ev.key.toLowerCase());
+});
+
+let direction: THREE.Vector3 = new THREE.Vector3();
 
 function animationLoop() : void {
     requestAnimationFrame(animationLoop);
-    controls.update();
+    fpc.update(1);
     renderer.render(scene, camera);
 }
 
 initialize();
 animationLoop();
-}catch(e) {
-    alert(e)
-}
